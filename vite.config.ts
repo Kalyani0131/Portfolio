26e2b773562cdda3fa6b05fed74bfc6434776c6a
Kalyani0131 <kalyani.vadakala@gmail.com>
2026-01-30 19:13:27 +0530

    Deploy build to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/',   
+  base: '/portfolio/',
+  build: {
+    outDir: 'docs',   
+  },
   plugins: [react()],
 });
 
